35d2993c51561f75839de5b4e3a48c910744df3c
Ritesh Singh <singhkuritesh1@gmail.com>
2023-10-14 10:32:35 +0530

    add init folder

---
+++ init/data.js
@@ -0,0 +1,46 @@
+const studentListings = [
+    {
+      name: "Rajesh Kumar",
+      branch: "Computer Science",
+      year: 4,
+      contributions: 15,
+      ratings: 4.5
+    },
+    {
+      name: "Priya Sharma",
+      branch: "Electrical Engineering",
+      year: 3,
+      contributions: 10,
+      ratings: 4.2
+    },
+    {
+      name: "Amit Patel",
+      branch: "Mechanical Engineering",
+      year: 3,
+      contributions: 12,
+      ratings: 4.1
+    },
+    {
+      name: "Neha Verma",
+      branch: "Civil Engineering",
+      year: 2,
+      contributions: 14,
+      ratings: 4.0
+    },
+    {
+      name: "Sandeep Singh",
+      branch: "Civil Engineering",
+      year: 4,
+      contributions: 8,
+      ratings: 3.9
+    },
+    {
+      name: "Swati Gupta",
+      branch: "Information Technology",
+      year: 3,
+      contributions: 12,
+      ratings: 3.8
+    }
+];  
+
+module.exports = {data: studentListings};
