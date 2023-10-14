588c3a6da09b0fbb4a4a00bcea98a902f4055b2f
Ritesh Singh <singhkuritesh1@gmail.com>
2023-10-14 10:41:51 +0530

    add index.js

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const mongoose = require("mongoose");
+const MONGO_URL = "mongodb://127.0.0.1:27017/ietnexus";
+const Listing = require("C:/Users/singh/IET Study/IETStudy/models/listing.js");
+
+main()
+    .then(()=>{
+        console.log("connected to DB");
+})
+    .catch((err)=>{
+        console.log(err);
+})
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
 
 app.use(express.urlencoded({extended: true}));
 
@@ -11,6 +26,16 @@ app.set("views", path.join(__dirname,"views"));
 app.get("/", (req, res)=>{
     res.render("home.ejs");
 });
+
+app.get("/home", (req, res)=>{
+    res.render("home.ejs");
+});
+
+app.get("/study-material", async (req, res)=>{
+    const studentListings = await Listing.find({});
+    res.render("study-material.ejs", {studentListings});
+})
+
 
 app.listen(port,()=>{
     console.log(`app is listening on port ${port}`);
